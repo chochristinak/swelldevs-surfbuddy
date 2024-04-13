dd2b0eecdf0d5e1ddfb6d2c988d650a389f40ec7
chock1991 <chochristinak@gmail.com>
2024-04-13 09:36:52 -0600

    Logo and link colors, icons

---
+++ main_app/static/js/main.js
@@ -0,0 +1,3 @@
+$.backstretch("https://images.unsplash.com/photo-1484260001192-bfe826505222?q=80&w=2064&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+
+
